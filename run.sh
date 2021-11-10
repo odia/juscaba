58647d35191384491622c5a2ccf5c43bca477e21
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-10 16:54:42 -0300

    use web INSDIE the container that is gonna get built

--- run.sh
+++ run.sh
@@ -3,23 +3,21 @@ set -Eeux
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 mkdir -p /tmp/juscaba/pdfs
-mkdir -p /tmp/juscaba/web/public/data
+mkdir -p public/data
 for ((i=1; i<=$#; i++))
 do
     exp=${!i}
     exp_filename=${!i/\//-}
 
-    ./builder "-json=/tmp/juscaba/web/public/data/${exp_filename}.json" -pdfs=/tmp/juscaba/pdfs "-expediente=${exp}" -images=false "-blacklist=${BLACKLIST_REGEX:-}"
+    ./builder "-json=public/data/${exp_filename}.json" -pdfs=/tmp/juscaba/pdfs "-expediente=${exp}" -images=false "-blacklist=${BLACKLIST_REGEX:-}"
 
     pushd ts
     yarn run ts-node create-index.ts /tmp/juscaba/web/public/data/${exp_filename}.json /tmp/juscaba/web/public/data/${exp_filename}-index.json
     popd
 done
 
-pushd web
 yarn
 yarn build
-popd
 
 mkdir -p /tmp/juscaba/web
 rm -rf /tmp/juscaba/web/build
