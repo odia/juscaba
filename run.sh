86597386032922e292811b6a40865d5626ec01a5
Sebastian Waisbrot <seppo0010@gmail.com>
2021-11-10 16:01:25 -0300

    Fix static files path

--- run.sh
+++ run.sh
@@ -2,19 +2,18 @@
 set -Eeux
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-mkdir -p ${SCRIPT_DIR}/web/public/data
+mkdir -p /tmp/juscaba/pdfs
+mkdir -p /tmp/juscaba/web/public/data
 for ((i=1; i<=$#; i++))
 do
     exp=${!i}
     exp_filename=${!i/\//-}
 
-    ./builder "-json=/tmp/juscaba/${exp_filename}.json" -pdfs=/tmp/juscaba/pdfs "-expediente=${exp}" -images=true "-blacklist=${BLACKLIST_REGEX:-}"
+    ./builder "-json=/tmp/juscaba/web/public/data/${exp_filename}.json" -pdfs=/tmp/juscaba/pdfs "-expediente=${exp}" -images=${PROCESS_IMAGES:-} "-blacklist=${BLACKLIST_REGEX:-}"
 
     pushd ts
-    yarn run ts-node create-index.ts /tmp/juscaba/${exp_filename}.json /tmp/juscaba/${exp_filename}-index.json
+    yarn run ts-node create-index.ts /tmp/juscaba/web/public/data/${exp_filename}.json /tmp/juscaba/web/public/data/${exp_filename}-index.json
     popd
-
-    cp /tmp/juscaba/${exp_filename}{-index,}.json ${SCRIPT_DIR}/web/public/data
 done
 
 yarn
